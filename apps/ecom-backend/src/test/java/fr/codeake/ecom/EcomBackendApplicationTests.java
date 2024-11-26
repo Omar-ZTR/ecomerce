560e1f185f640b76869b6df194947c3ddaeabd44
Omar zouiter <omarzouiter97@gmail.com>
2024-11-26 19:08:12 +0100

    ecomOn

---
+++ apps/ecom-backend/src/test/java/fr/codeake/ecom/EcomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.codeake.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
